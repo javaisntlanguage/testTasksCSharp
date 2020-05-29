4858cd4d299811e153bbbac0ab5f8daaabaebb71
pasratel1 <artajove@list.ru>
2020-05-29 22:27:05 +0400

    задание 3: вынес функцию TryParse в отдельный класс

---
+++ Desktop/3 курс/testTask/task3/task3/Parser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task3
+{
+    static class Parser
+    {
+        //финкция определения наименьшего типа 
+        public static dynamic TryParse(string str)
+        {
+            sbyte sByte = 0;
+            short Short = 0;
+            int Int = 0;
+            double Double = 0;
+            long Long = 0;
+            if (sbyte.TryParse(str, out sByte))
+            {
+                return sByte;
+            }
+            else if (short.TryParse(str, out Short))
+            {
+                return Short;
+            }
+            else if (int.TryParse(str, out Int))
+            {
+                return Int;
+            }
+            else if (long.TryParse(str, out Long))
+            {
+                return Long;
+            }
+            else if (double.TryParse(str, out Double))
+            {
+                return Double;
+            }
+            return str;
+        }
+    }
+}
--- Desktop/3 курс/testTask/task3/task3/Program.cs
+++ Desktop/3 курс/testTask/task3/task3/Program.cs
@@ -32,7 +32,7 @@ namespace task3
                             for(int j=0;j<words.Length;j++)
                             {
                                 //задаем правильный тип и заменяем символы '_' на пробел
-                                wordsAnyType[j] = TryParse(words[j].Replace('_',' '));
+                                wordsAnyType[j] = Parser.TryParse(words[j].Replace('_',' '));
                                 //если не заголовок и не номер, то выводим с типом
                                 if (i != 0 && j != 0) 
                                 { 
@@ -70,37 +70,6 @@ namespace task3
             {
                 Console.WriteLine(e.Message);
             }
-        }
-
-        //финкция определения наименьшего типа 
-        static dynamic TryParse(string str)
-        {
-            sbyte sByte = 0;
-            short Short = 0;
-            int Int = 0;
-            double Double = 0;
-            long Long = 0;
-            if(sbyte.TryParse(str,out sByte))
-            {
-                return sByte;
-            }
-            else if(short.TryParse(str,out Short))
-            {
-                return Short;
-            }
-            else if(int.TryParse(str,out Int))
-            {
-                return Int;
-            }
-            else if (long.TryParse(str, out Long))
-            {
-                return Long;
-            }
-            else if(double.TryParse(str,out Double))
-            {
-                return Double;
-            }
-            return str;
         }
     }
 }
